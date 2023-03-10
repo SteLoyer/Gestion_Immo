88b736b14ad8cd044716681a8a5c025da7566cc2
Stephane Loyer <s.loyer@free.fr>
2023-03-10 23:48:20 +0100

    Création du projet

---
+++ App_Gestion_Immo/App_Gestion_Immo/App_Gestion_Immo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace App_Gestion_Immo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
